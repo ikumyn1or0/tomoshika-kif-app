678c5e8919b0d8171f9224cc8b519b97dea2810c
ikumyn1or0 <19ming3@gmail.com>
2025-07-19 20:41:14 +0900

    [update]ruff/mypyを使ったlintingを適用

--- app.py
+++ app.py
@@ -1,38 +1,79 @@
-import streamlit as st
-import pandas as pd
-import requests
 import re
 
+import pandas as pd
+import requests
+import streamlit as st
 
 CSV_URL = "https://drive.google.com/uc?id=1Lg1xB79PYue1D5qSPy2IMo6zNMjOgEa4&export=download"
 
+
 @st.cache_data(ttl=300)
-def load_index_csv():
-    df = pd.read_csv(CSV_URL)
-    return df
+def load_index_csv() -> pd.DataFrame:
+    """index.csvを読み込みキャッシュにためておく関数
+
+    Returns:
+        pd.DataFrame
+
+    """
+    return pd.read_csv(CSV_URL)
+
 
 def extract_file_id(drive_url: str) -> str | None:
-    print(drive_url)
+    """Google driveのurlからファイルidを取得する
+
+    Args:
+        drive_url: str
+            google driveのurl
+
+    Returns:
+        str | None
+            ファイルid
+
+    """
     match = re.search(r"/d/([a-zA-Z0-9_-]+)", drive_url)
     if match:
         return match.group(1)
     return None
 
+
 @st.cache_data()
-def load_kif_text(kif_url):
+def load_kif_text(kif_url: str) -> str:
+    """棋譜データのテキストをurlから取得する
+
+    Args:
+        kif_url: str
+            棋譜データのurl
+
+    Returns:
+        str
+            kif形式の棋譜データ
+
+    """
     kif_if = extract_file_id(kif_url)
-    response = requests.get(f"https://drive.google.com/uc?id={kif_if}&export=download")
+    response = requests.get(f"https://drive.google.com/uc?id={kif_if}&export=download", timeout=300)
     response.raise_for_status()
     return response.text
 
-def get_match_text(df, i):
-    date = df.loc[i, "日付"]
-    no = df.loc[i, "試合番号"]
-    sente = df.loc[i, "先手"]
-    gote = df.loc[i, "後手"]
-    result = df.loc[i, "結果"]
-    return f"{date} {no}局目 {sente} 対 {gote} ({result})"
 
+def get_match_text(df: pd.DataFrame, index: int) -> str:
+    """指定されたindexの対局情報をテキストとして受け取る
+
+    Args:
+        df: pd.DataFrame
+            対局情報を格納したテーブル
+        index: int
+            対局情報を返したいテーブルのindex
+
+    Returns:
+        str: _description_
+
+    """
+    date = df.loc[index, "日付"]
+    no = df.loc[index, "試合番号"]
+    sente = df.loc[index, "先手"]
+    gote = df.loc[index, "後手"]
+    result = df.loc[index, "結果"]
+    return f"{date} {no}局目 {sente} 対 {gote} ({result})"
 
 
 st.title("トモシカ対局棋譜データ")
@@ -45,7 +86,7 @@ col = st.columns([4, 1])
 selected_index = st.selectbox(
     "対局を選択",
     sorted(df.index, reverse=True),
-    format_func=lambda i: get_match_text(df, i)
+    format_func=lambda i: get_match_text(df, i),
 )
 
 row = df.loc[selected_index]
@@ -61,8 +102,14 @@ st.markdown(f"""
 try:
     kif_text = load_kif_text(row["棋譜データURL"])
     st.text_area("棋譜（KIF形式）", kif_text, height=300)
-    st.download_button("棋譜をダウンロード", kif_text, file_name=get_match_text(df, selected_index)+".txt", mime="text/plain", type="primary")
-except Exception as e:
+    st.download_button(
+        "棋譜をダウンロード",
+        kif_text,
+        file_name=get_match_text(df, selected_index) + ".txt",
+        mime="text/plain",
+        type="primary",
+    )
+except FileNotFoundError as e:
     st.error(f"棋譜の読み込みに失敗しました: {e}")
 
 
